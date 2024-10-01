c62d66d7370a3b489bff27ccc833d25af8f4c305
Isabela Carone <isacaronelacerda@gmail.com>
2024-10-01 15:04:26 -0300

    Update App.js

--- primeira_aula_06_08/App.js
+++ primeira_aula_06_08/App.js
@@ -1,15 +1,17 @@
 // código da primeira aula mobile 06/08
 
 import { SafeAreaView, Text, View } from 'react-native';
+// importando as características do react-native, precisamos declarar de onde estamos importando elas 
 
 import AppStyles from './styles/AppStyles'
+// qual pasta estamos pegando elas; './style/AppStyles' ./pasta/arquivo
 
 export default function App() {
   return (
-    <View style={ AppStyles.container }>
-      <View style={ AppStyles.cabecalho }>
+    <View style={ AppStyles.container }> 
+      <View style={ AppStyles.cabecalho }> 
         <Text>
-          header
+          header 
         </Text>
       </View>
       <View style={ AppStyles.corpo }>
@@ -26,4 +28,8 @@ export default function App() {
   );
 }
 
+// seguimos o padrão de hierarquia normal de códigos com as chaves abertas e suas sucessões
 
+// o text exibe o texto na tela e o view é um container padrão do react, ele é usado para estilizar uma View (a qual foi citada no import)
+
+// e também determinamos o styles abrino chaves e selecionando a classe referente ao estilo a ser usado
